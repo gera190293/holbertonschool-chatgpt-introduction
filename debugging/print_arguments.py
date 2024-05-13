b77e4752823f261f1ea8b7eb0d1e02205f3cb3a8
gera190293 <gera190293@gmail.com>
2024-05-12 23:51:42 -0300

    ChatGptDebugging

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-# Start the loop from index 1, as sys.argv[0] contains the script name
-for i in range(1, len(sys.argv)):
-    print(sys.argv[i])
+for arg in sys.argv[1:]:
+    print(arg)
